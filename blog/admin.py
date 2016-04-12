99769533c198f3dff20a77bb223ad0812b087a1f
Dan Starner <starner.daniel5@gmail.com>
2016-04-12 16:04:45 -0400

    Save to MD file

--- blog/admin.py
+++ blog/admin.py
@@ -10,7 +10,7 @@ class PostModelForm(forms.ModelForm):
 
     class Meta:
         model = Post
-        exclude = ['identity', 'html_file']
+        exclude = ['identity', 'posted_at', 'markdown_file']
 
 
 class PostAdmin(admin.ModelAdmin):
--- blog/models.py
+++ blog/models.py
@@ -1,5 +1,8 @@
 import datetime
+import os
+
 import requests
+from django.conf import settings
 from django.core.files.base import ContentFile
 from lib import random_generator
 from django.contrib.auth.models import User
@@ -12,11 +15,21 @@ class Post(Model):
     author = ForeignKey(User, on_delete=CASCADE, related_name="posts")
     markdown = TextField(default="")
     posted_at = DateField(default=datetime.date.today)
+    markdown_file = FileField(upload_to="blog/markdown/", null=True, blank=True)
     identity = CharField(max_length=16, default=random_generator)
 
     def display_html(self):
         with open(self.html_file.path, encoding='utf-8') as f:
             return f.read()
+
+    def save(self, *args, **kwargs):
+        filename = self.title.replace(" ", "-") + "*.md"
+        if self.markdown_file:
+            self.markdown_file.delete(False)
+        self.markdown_file.save(filename, ContentFile(self.markdown), save=False)
+        # TODO: Call metalsmith instead of echo
+        os.system("echo " + os.path.join(settings.BASE_DIR, 'media/blog/markdown/' + filename))
+        super(Post, self).save(*args, **kwargs)
 
     def __str__(self):
         return self.title
