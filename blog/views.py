25b8375786a83a746fbb917fb0224c4120df5156
Dan Starner <starner.daniel5@gmail.com>
2016-04-30 16:27:25 -0400

    Pagination added

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,5 @@
 from django.contrib import messages
+from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
 from django.http import HttpResponse
 from django.shortcuts import render, redirect
 from django.template import Template, Context
@@ -9,7 +10,17 @@ from blog.models import Post
 
 def index(request):
     posts = Post.objects.all()
-    return render(request, 'blog.html', {'posts': posts})
+    paginator = Paginator(posts, 15)
+    page = int(request.GET.get('page', 1))
+    try:
+        blogs = paginator.page(page)
+    except PageNotAnInteger:
+        # If page is not an integer, deliver first page.
+        blogs = paginator.page(1)
+    except EmptyPage:
+        # If page is out of range (e.g. 9999), deliver last page of results.
+        blogs = paginator.page(paginator.num_pages)
+    return render(request, 'blog.html', {'posts': blogs})
 
 
 def view_post(request, identity):
